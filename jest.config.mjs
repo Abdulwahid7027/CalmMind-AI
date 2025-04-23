c0c368bfc2aa394a615680f3796db966113196af
Shaik Abdul Wahid <haseena7027@gmail.com>
2025-04-24 04:27:11 +0530

    update

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,12 +1,12 @@
-// jest.config.mjs
 export default {
-    testEnvironment: 'jest-environment-jsdom',
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass|svg)$': 'identity-obj-proxy',
-    },
-  };
-  
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+  // OR
+  // testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass|svg)$': 'identity-obj-proxy',
+  },
+};
